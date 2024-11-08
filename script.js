8eca6a5f2cbf41f104cdfcee34d9f4169e7d5079
emclysm <e1long@torontomu.ca>
2024-11-08 17:05:02 -0500

    sdfs

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 document.getElementById("nameButton").addEventListener("click", function() {
     const name = prompt("Please enter your name:");
     if (name) {
-        document.getElementById("nameOutput").innerText = "Hello " + name + ",";
+        document.getElementById("nameOutput").innerText = "Hello " + name + "! ᵕ̈";
     }
 });
 
